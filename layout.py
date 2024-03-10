2093f8997ac1b0f5755ff74e494bd3c9ff0613cf
Ashish Upadhyay <102135235+AplicanU@users.noreply.github.com>
2024-03-10 06:52:36 +0000

    refactor 15

--- layout.py
+++ layout.py
@@ -1,6 +1,7 @@
 import chainlit as cl
 from backend import healthily, login
 import json
+from fpdf import FPDF
 
 
 
@@ -25,47 +26,9 @@ user_agreed = False
 
 all_ids = []          # List to store all available IDs
 
-
-
-
-async def conversation():
-    # Iterate over questions
-    for key, question in initial_questions.items():
-        answer = await cl.AskUserMessage(content=question).send()
-        user_answers[key] = answer['output'] if answer else None
-
-    # Storing user answers in variables
-    user_name =  user_answers["name"]
-    user_gender = user_answers["gender"]
-    user_yob = user_answers["yob"]
-    user_symptoms = user_answers["symptoms"]
-    #user_medical_history = user_answers["medical_history"]
-
-    global user_details
-
-    user_details = ','.join(user_name)
-    user_details = ','.join(user_gender)
-    user_details = ','.join(user_yob)
-    user_details = ','.join(user_symptoms)
-
-    # Do something with the stored user answers
-    #print("User's Details:", user_name, user_gender, user_yob)
-    #print("User's Symptoms:", user_symptoms)
-    #print("User's Medical History:", user_medical_history)
-    api = healthily.HealthilyApi()
-    response = await api.start_conversation(user_name, user_gender, user_yob, user_symptoms)
-    # Parse JSON data
-    #data = json.loads(response)
-
-    # Extract question and choices
-    while 'report' not in response:
-        new_reponse = await next_question(response, api)
-        response = new_reponse
-       
-
 async def next_question(response, api):
     global all_ids
-    if response['question']['type'] in ["name","health_background","factor", "symptoms"]:
+    if response['question']['type'] in ["name","health_background","factor", "symptoms", "symptom"]:
         question = ' '.join([question['text'] for question in response['question']['messages']])
     else:
         question = ' '.join([question['value'] for question in response['question']['messages']])
@@ -75,7 +38,7 @@ async def next_question(response, api):
         choice_value = 'label'
         if response['question']['type'] == 'health_background':
             choice_value = 'long_name'
-        elif response['question']['type'] in ["factor","symptoms"]:
+        elif response['question']['type'] in ["factor","symptoms", "symptom"]:
             choice_value = 'text'
         choices = {choice['id']: choice[choice_value] for choice in response['question']['choices']}
         options = ', '.join(choices.values())
@@ -112,6 +75,91 @@ async def next_question(response, api):
     response = await api.respond_to_healthily(chosen_ids, not_chosen_ids, answer_type)
 
     return response
+
+async def parse_report(response):
+    name = response['user']['name']
+    gender = response['user']['gender']
+    age = response['user']['age']
+    #message = ' '.join([message['value'] for message in response['question']['messages']])
+    diagnosis_possible = response['report']['summary']['diagnosis_possible']
+    main_symptoms = [symptom['name'] for symptom in response['report']['summary']['extracted_symptoms']]
+    duration = response['report']['summary']['duration']
+    additional_symptoms = [symptom['name'] for symptom in response['report']['summary']['additional_symptoms']]
+    unsure_symptoms = [symptom['name'] for symptom in response['report']['summary']['unsure_symptoms']]
+    advice = response['report']['summary']['consultation_triage']['triage_advice']
+    advice_level = response['report']['summary']['consultation_triage']['level']
+    #articles = response['report']['summary']['articles_v3']['content']
+    influencing_factors = []
+    for inf_factor in response['report']['summary']['influencing_factors']:
+        if inf_factor['value']['value'] == True:
+            influencing_factors.append(inf_factor['long_name'])
+
+    #show report
+    actions = [
+        cl.Action(name="Download Report", value="Download", label="Download", description="Download Report")
+    ]
+    content = "Please find your consulation report below: \n\n Name: " + name + "\n Gender: " + gender + "\n Age: " + str(age) + "\n\n Diagnosis Possible: " + str(diagnosis_possible) + "\n Main Symptoms: " + str(main_symptoms) + "\n Duration of Main Symptoms: " + duration + "\n\n Additional Symptoms: " + str(additional_symptoms) + "\n Unsure Symptoms: " + str(unsure_symptoms) + "\n\n Advice: " + advice + "\n Advice Level: " + advice_level + "\n Influencing Factors: " + str(influencing_factors)
+    await cl.Message(content=content, actions=actions).send()
+
+@cl.action_callback("Download Report")
+async def download_pdf(content):
+    # Create an instance of the FPDF class
+    pdf = FPDF()
+
+    # Add a page
+    pdf.add_page()
+
+    # Set the font (you can choose other fonts as well)
+    pdf.set_font("Arial", size=15)
+
+    # Insert a cell with the title
+    pdf.cell(200, 10, txt="COnsultation Report", ln=1, align='C')
+
+    # Insert a cell with the description
+    pdf.cell(200, 10, txt=content, ln=2, align='C')
+
+    # Save the PDF with the filename "GFG.pdf"
+    pdf.output("report.pdf")
+
+async def conversation():
+    # Iterate over questions
+    for key, question in initial_questions.items():
+        answer = await cl.AskUserMessage(content=question).send()
+        user_answers[key] = answer['output'] if answer else None
+
+    # Storing user answers in variables
+    user_name =  user_answers["name"]
+    user_gender = user_answers["gender"]
+    user_yob = user_answers["yob"]
+    user_symptoms = user_answers["symptoms"]
+    #user_medical_history = user_answers["medical_history"]
+
+    global user_details
+
+    user_details = ','.join(user_name)
+    user_details = ','.join(user_gender)
+    user_details = ','.join(user_yob)
+    user_details = ','.join(user_symptoms)
+
+    # Do something with the stored user answers
+    #print("User's Details:", user_name, user_gender, user_yob)
+    #print("User's Symptoms:", user_symptoms)
+    #print("User's Medical History:", user_medical_history)
+    api = healthily.HealthilyApi()
+    response = await api.start_conversation(user_name, user_gender, user_yob, user_symptoms)
+    # Parse JSON data
+    #data = json.loads(response)
+
+    # Extract question and choices
+    while 'report' not in response:
+        new_reponse = await next_question(response, api)
+        response = new_reponse
+
+    #Generate report
+    if 'report' in response:
+        await parse_report(response)
+       
+
 
 '''for choice_id, choice_label in choices.items():
         action = cl.Action(name="Further_Actions", value=choice_id, label=choice_label, description=choice_label)
--- test.py
+++ test.py
@@ -1,10 +1,38 @@
-#Change the chatbot icon
-@cl.set_chat_profiles
-async def chat_profile():
-    return [
-    cl.ChatProfile(
-            name="Zenith Care",
-            #markdown_description="The underlying model is **GPT-3.5**.",
-            icon="/public/favicon.png",
-        )
-    ]
+import chainlit as cl
+import json
+
+def parse_report(response):
+    name = response['user']['name']
+    gender = response['user']['gender']
+    age = response['user']['age']
+    #message = ' '.join([message['value'] for message in response['question']['messages']])
+    diagnosis_possible = response['report']['summary']['diagnosis_possible']
+    main_symptoms = [symptom['name'] for symptom in response['report']['summary']['extracted_symptoms']]
+    duration = response['report']['summary']['duration']
+    additional_symptoms = [symptom['name'] for symptom in response['report']['summary']['additional_symptoms']]
+    unsure_symptoms = [symptom['name'] for symptom in response['report']['summary']['unsure_symptoms']]
+    advice = response['report']['summary']['consultation_triage']['triage_advice']
+    advice_level = response['report']['summary']['consultation_triage']['level']
+    #articles = response['report']['summary']['articles_v3']['content']
+    influencing_factors = []
+    for inf_factor in response['report']['summary']['influencing_factors']:
+        if inf_factor['value']['value'] == True:
+            influencing_factors.append(inf_factor['long_name'])
+
+    #show report
+    
+    content = "Please find your consulation report below: \n\n Name: " + name + "\n Gender: " + gender + "\n Age: " + str(age) + "\n Diagnosis Possible: " + str(diagnosis_possible) + "\n Main Symptoms: " + str(main_symptoms) + "\n Duration of Main Symptoms: " + duration + "\n Additional Symptoms: " + str(additional_symptoms) + "\n Unsure Symptoms: " + str(unsure_symptoms) + "\n Advice: " + advice + "\n Advice Level: " + advice_level + "\n Influencing Factors: " + str(influencing_factors)
+    
+    print(content)
+
+
+
+with open("report2.json", "r") as read_file:
+    response = json.load(read_file)
+
+
+def main():
+    parse_report(response)
+
+if __name__ == "__main__":
+    main()
